4822aa54773985ad202397d3f2c9c902587429a9
Timo Sinnaeve <timo.sinnaeve1276@gmail.com>
2026-03-26 09:51:05 +0100

    updated uitbreiding

---
+++ Prototypes and simulations/Finished Prototype/LED Strips/Peripheral_Strips_Compleet/Peripheral_Strips_Compleet.ino
@@ -0,0 +1,137 @@
+#include <ArduinoBLE.h>
+#include <Arduino_BMI270_BMM150.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN_LEFT    2
+#define PIN_RIGHT   3
+#define PIN_BRAKE   4
+#define NUM_LEDS    16 
+
+Adafruit_NeoPixel stripL(NUM_LEDS, PIN_LEFT,  NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripR(NUM_LEDS, PIN_RIGHT, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripB(NUM_LEDS, PIN_BRAKE, NEO_GRB + NEO_KHZ800);
+
+uint32_t orange = stripL.Color(255, 60, 0);
+uint32_t red    = stripL.Color(255, 0, 0);
+uint32_t black  = stripL.Color(0, 0, 0);
+
+unsigned long brakeConditionStarted = 0;
+unsigned long redStartTime = 0;
+const long duration = 1000;
+const int brakeDelay = 100;
+
+bool blinkLinker = false;
+bool blinkRechter = false;
+unsigned long lastBlinkTime = 0;
+const int blinkInterval = 500;
+bool ledToggleState = false;
+
+uint32_t lastColorB = 0;
+uint32_t lastColorL = 0;
+uint32_t lastColorR = 0;
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+BLEService customService(deviceServiceUuid); 
+BLEByteCharacteristic dataCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  stripL.begin(); stripR.begin(); stripB.begin();
+  stripL.show();  stripR.show();  stripB.show();
+
+  if (!IMU.begin()) { Serial.println("IMU fail!"); while (1); }
+  if (!BLE.begin()) { Serial.println("BLE fail!"); while (1); }
+
+  BLE.setLocalName("Nano 33 BLE Peripheral");
+  BLE.setAdvertisedService(customService);
+  customService.addCharacteristic(dataCharacteristic);
+  BLE.addService(customService);
+  dataCharacteristic.writeValue(0);
+  BLE.advertise();
+}
+
+void loop() {
+  float ax, ay, az;
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    while (central.connected()) {
+      unsigned long currentTime = millis();
+      digitalWrite(LED_BUILTIN, HIGH);
+
+      // --- 1. BRAKE LIGHT LOGIC ---
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(ax, ay, az);
+
+        if (ax < -0.3) {
+          if (brakeConditionStarted == 0) {
+            brakeConditionStarted = currentTime;
+          }
+          if (currentTime - brakeConditionStarted >= brakeDelay) {
+            redStartTime = currentTime;
+          }
+        } else {
+          brakeConditionStarted = 0;
+        }
+      }
+
+      if (redStartTime > 0 && (currentTime - redStartTime < duration)) {
+        updateStrip(stripB, red, lastColorB);
+      } else {
+        updateStrip(stripB, black, lastColorB);
+        if (currentTime - redStartTime >= duration) redStartTime = 0;
+      }
+
+      // --- 2. BLINKING LOGIC ---
+      if (currentTime - lastBlinkTime >= blinkInterval) {
+        lastBlinkTime = currentTime;
+        ledToggleState = !ledToggleState;
+
+        if (blinkLinker) {
+          updateStrip(stripL, ledToggleState ? orange : black, lastColorL);
+          updateStrip(stripR, black, lastColorR);
+        } 
+        else if (blinkRechter) {
+          updateStrip(stripR, ledToggleState ? orange : black, lastColorR);
+          updateStrip(stripL, black, lastColorL);
+        }
+        else {
+          updateStrip(stripL, black, lastColorL);
+          updateStrip(stripR, black, lastColorR);
+        }
+      }
+
+      if (dataCharacteristic.written()) {
+        handleIncomingData(dataCharacteristic.value());
+      }
+    }
+  }
+  
+  blinkRechter = false;
+  blinkLinker = false;
+  updateStrip(stripL, black, lastColorL);
+  updateStrip(stripR, black, lastColorR);
+  updateStrip(stripB, black, lastColorB);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void updateStrip(Adafruit_NeoPixel &s, uint32_t newColor, uint32_t &lastStoredColor) {
+  if (newColor != lastStoredColor) {
+    for(int i=0; i<NUM_LEDS; i++) {
+      s.setPixelColor(i, newColor);
+    }
+    s.show();
+    lastStoredColor = newColor;
+  }
+}
+
+void handleIncomingData(byte value) {
+  switch (value) {
+    case 1: blinkLinker = !blinkLinker; if (blinkLinker) blinkRechter = false; break;
+    case 3: blinkRechter = !blinkRechter; if (blinkRechter) blinkLinker = false; break;
+  }
+}
